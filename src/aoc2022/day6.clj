420ab64c1d920476138c1e0e0b43fdea6fab47c0
Albert Lai <kayulai2@gmail.com>
2022-12-06 12:51:55 +0000

    better naming

--- src/aoc2022/day6.clj
+++ src/aoc2022/day6.clj
@@ -1,20 +1,20 @@
 (ns aoc2022.day6)
 
 
-(defn first-marker
-  [s num-of-uniq]
+(defn distinct-char-marker
+  [s distinct-n]
   (->> s
        (seq)
        (reduce
          (fn [[processed remaining marker] ch]
            (let [processed' (conj processed ch)
-                 last-n (take-last num-of-uniq processed')
+                 last-n (take-last distinct-n processed')
                  uniq-count
                  (count (set last-n))]
              (cond
                marker
                [processed (conj remaining ch) true]
-               (= num-of-uniq uniq-count)
+               (= distinct-n uniq-count)
                [processed' remaining true]
                :else
                [processed' remaining false])))
@@ -22,41 +22,41 @@
 
 
 (comment
-  (first-marker "bvwbjplbgvbhsrlpgdmjqwftvncz" 4)
-  (first-marker "nppdvjthqldpwncqszvftbrmjlhg" 4)
-  (first-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 4)
-  (first-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 4))
+  (distinct-char-marker "bvwbjplbgvbhsrlpgdmjqwftvncz" 4)
+  (distinct-char-marker "nppdvjthqldpwncqszvftbrmjlhg" 4)
+  (distinct-char-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 4)
+  (distinct-char-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 4))
 
 
 ;; part 1
 
-(defn char-after-marker
+(defn start-of-packet-marker
   []
-  (-> (first-marker (slurp "resources/day6.txt") 4)
+  (-> (distinct-char-marker (slurp "resources/day6.txt") 4)
       (first)
       (count)))
 
 
 (comment
-  (char-after-marker))
+  (start-of-packet-marker))
 
 
 ;; part 2
 
-(defn char-after-message-marker
+(defn start-of-message-marker
   []
-  (-> (first-marker (slurp "resources/day6.txt") 14)
+  (-> (distinct-char-marker (slurp "resources/day6.txt") 14)
       (first)
       (count)))
 
 
 (comment
-  (first-marker "mjqjpqmgbljsphdztnvjfqwrcgsmlb" 14)
-  (first-marker "bvwbjplbgvbhsrlpgdmjqwftvncz" 14)
-  (first-marker "nppdvjthqldpwncqszvftbrmjlhg" 14)
-  (first-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 14)
-  (first-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 14)
-  (char-after-message-marker))
+  (distinct-char-marker "mjqjpqmgbljsphdztnvjfqwrcgsmlb" 14)
+  (distinct-char-marker "bvwbjplbgvbhsrlpgdmjqwftvncz" 14)
+  (distinct-char-marker "nppdvjthqldpwncqszvftbrmjlhg" 14)
+  (distinct-char-marker "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" 14)
+  (distinct-char-marker "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" 14)
+  (start-of-message-marker))
 
 
 ;; done
